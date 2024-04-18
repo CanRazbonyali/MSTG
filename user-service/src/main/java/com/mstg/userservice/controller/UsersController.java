65f76ea0f45c03367a0abf567396ca52a962d9b7
Can RAZBONYALI <canrazbonyali@gmail.com>
2024-04-18 19:47:01 +0300

    User service çalışması

--- user-service/src/main/java/com/mstg/userservice/controller/UsersController.java
+++ user-service/src/main/java/com/mstg/userservice/controller/UsersController.java
@@ -1,10 +1,49 @@
 package com.mstg.userservice.controller;
 
+import com.mstg.userservice.dto.LoginDto;
+import com.mstg.userservice.model.Users;
+import com.mstg.userservice.model.Users_Details;
+import com.mstg.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
 * TODO: to be implemented...
+*  Spring Security -> JWT (json web token)
 * */
 @RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UsersController {
+    private final Logger _logger = LoggerFactory.getLogger(UsersController.class);
+    private final UserService _userService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginDto dto) {
+        boolean result = _userService.login(dto);
+        if (result) {
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.internalServerError().build(); ///!!!!
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity login() {
+        Users demoUser = Users.builder().username("can").password("123")
+                .detail(Users_Details.builder()
+                        .age(20)
+                        .gender("male")
+                        .address("İstanbul")
+                        .build())
+                .build();
+
+        return ResponseEntity.ok().build();
+    }
 }
---
+++ user-service/src/main/java/com/mstg/userservice/service/impl/UserService_Impl.java
@@ -0,0 +1,48 @@
+package com.mstg.userservice.service.impl;
+
+import com.mstg.userservice.dto.LoginDto;
+import com.mstg.userservice.model.Users;
+import com.mstg.userservice.repository.UserRepository;
+import com.mstg.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService_Impl implements UserService {
+    private final UserRepository _userRepository;
+
+    @Override
+    public boolean register() {
+        try {
+
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean login(LoginDto dto) {
+        try {
+            Optional<Users> optUser = _userRepository.findByUsername(dto.getUsername());
+
+            if (optUser.isEmpty())
+                throw new Exception("Resource not found");
+
+            Users user = optUser.get();
+            if (user.getPassword().equals(dto.getPassword())) {
+                return true;
+                /**
+                 * login işlemi olacak
+                 * */
+            } else
+                return false;
+
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+}
